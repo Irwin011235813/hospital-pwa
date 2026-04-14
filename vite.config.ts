1c140760a4ca1126866ff3b7af50623b582f999b
Irwin Mor <011235813ir@gmail.com>
2026-04-14 05:19:25 -0300

    Configuración de alias @ en Vite y TS

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,12 @@
-import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path' // Necesitás importar esto
 
 export default defineConfig({
   plugins: [react()],
-
-  server: {
-    port: 3000,
-    open: true,
-    host: true
-  },
-
   resolve: {
-    alias: { '@': path.resolve(__dirname, 'src') },
+    alias: {
+      '@': path.resolve(__dirname, './src'), // Aquí le decís qué significa el @
+    },
   },
 })
